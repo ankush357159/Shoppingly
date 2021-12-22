3e262bbf28d733f45aeffca7fbbe1b3ad10fd69f
Ankush <ankush357159@gmail.com>
2021-12-22 17:07:23 +0530

    Registration Login

--- app/views.py
+++ app/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+from django.views import View
+from app.forms import CustomerRegistrationForm
+from .models import Customer, Cart, Product, OrderPlaced
+from django.contrib import messages
 
-def home(request):
- return render(request, 'app/home.html')
+# def home(request):
+#  return render(request, 'app/home.html')
+class ProductView(View):
+    def get(self, request):
+        topwears = Product.objects.filter(category='TW')
+        bottomwears = Product.objects.filter(category='BW')
+        mobiles = Product.objects.filter(category='M')
+        return render(request, 'app/home.html', {'topwears':topwears, 'bottomwears': bottomwears, 'mobiles': mobiles})
 
-def product_detail(request):
- return render(request, 'app/productdetail.html')
+
+
+# def product_detail(request):
+#  return render(request, 'app/productdetail.html')
+
+class ProductDetailView(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        return render(request, 'app/productdetail.html', {'product': product})
 
 def add_to_cart(request):
  return render(request, 'app/addtocart.html')
@@ -21,17 +38,39 @@ def address(request):
 def orders(request):
  return render(request, 'app/orders.html')
 
-def change_password(request):
- return render(request, 'app/changepassword.html')
+# def change_password(request):
+#  return render(request, 'app/changepassword.html')
 
-def mobile(request):
- return render(request, 'app/mobile.html')
+# def mobile(request):
+#  return render(request, 'app/mobile.html')
+def mobile(request, data=None):
+    if data == None:
+        mobiles = Product.objects.filter(category='M')
+    elif data == 'Tecno' or data == 'Redmi' or data == 'Samsung' or data == 'RealMe':
+        mobiles = Product.objects.filter(category='M').filter(brand=data)
+    elif data == 'below':
+        mobiles = Product.objects.filter(category='M').filter(discounted_price__lt=10000)
+    elif data == 'above':
+        mobiles = Product.objects.filter(category='M').filter(discounted_price__gt=10000)        
+    return render(request, 'app/mobile.html', {'mobiles':mobiles})
 
 def login(request):
  return render(request, 'app/login.html')
 
-def customerregistration(request):
- return render(request, 'app/customerregistration.html')
+# def customerregistration(request):
+#  return render(request, 'app/customerregistration.html')
+
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, 'app/customerregistration.html', {'form':form})
+
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Congratulations!! Registration successfull')
+            form.save()
+        return render(request, 'app/customerregistration.html', {'form':form})
 
 def checkout(request):
  return render(request, 'app/checkout.html')
